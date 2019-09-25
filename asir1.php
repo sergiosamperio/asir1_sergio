00d852eabf58a38966e03eaec90e4a51c0813d49
sergiosamperio <sergiosampe9@gmail.com>
2019-09-25 10:49:09 +0200

    version timestamp

--- asir1.php
+++ asir1.php
@@ -1,9 +1,12 @@
 <?php
+echo "<a href='http://192.168.0.91/asir1_javi/asir1.php'>Javi</a>";
+echo "</br>";
 echo '<a>Sergio Samperio Manteca</a>';
 echo "</br>";
 echo "<a href='http://192.168.0.60/asir1_luis/asir1.php'>Luis Gomez</a>";
 $file = fopen("visitas.txt","a");
 $ip=$_SERVER['REMOTE_ADDR'];
+fwrite($file, date('Y-m-d H:i '));
 echo fwrite($file,$ip);
 fclose($file);
 ?>
