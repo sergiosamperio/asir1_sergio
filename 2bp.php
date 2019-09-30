fe1cfb2eace20653375ba0f389acebe9a9412c44
sergiosamperio <sergiosampe9@gmail.com>
2019-09-30 12:14:24 +0200

    version parametros

---
+++ 2bp.php
@@ -0,0 +1,10 @@
+ <?php
+ function calcula ($a, $b) {
+	echo 'Suma'.$a.'+'.$b.'=' . ($a+$b) . '</br>';
+	echo 'Resta'.$a.'-'.$b.'=' . ($a-$b) . '</br>';
+	echo 'Producto'.$a.'*'.$b.'=' . ($a*$b) . '</br>';
+	echo 'División'.$a.'/'.$b.'=' . ($a/$b) . '</br>';
+}
+calcula (12,2);
+calcula (24,4);
+?>
