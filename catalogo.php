5dd62f9e6bd36dba1835844b3fa6d06fafbb93ed
sergiosamperio <sergiosampe9@gmail.com>
2019-09-30 10:18:08 +0200

    version catalogo

---
+++ catalogo.php
@@ -0,0 +1,20 @@
+<body>
+<a href="https://github.com/dblancoformacion" target="_blank">PROFE</a></br>
+<a href="https://github.com/AaronASIR/Asir1_aaron" target="_blank">Aaron</a></br>
+<a href="https://github.com/Real6/ASIR1_Ruben" target="_blank">Ruben</a></br>
+<a href="https://github.com/victor-zayas" target="_blank">Victor Z</a></br>
+<a href="https://github.com/deivisg98" target="_blank">David G</a></br>
+<a href="https://github.com/pablomoreta/asir1_pablo.git" target="_blank">Pablo</a></br>
+<a href="https://github.com/adrivg19/asir1_adrian" target="_blank">Adrian</a></br>
+<a href="https://github.com/MarioAlonso31info/asir1_mario" target="_blank">Mario</a></br>
+<a href="https://github.com/bruno38pla/ASIR1_BRUNO" target="_blank">Bruno</a></br>
+<a href="https://github.com/AlejandroGoVi/asir1_alex" target="_blank">Alex</a></br>
+<a href="https://github.com/carlosccsan/asir1-cc" target="_blank">Carlos</a></br>
+<a href="https://github.com/gargiljavi" target="_blank">Javi</a></br>
+<a href="https://github.com/sergiosamperio/asir1_sergio" target="_blank">Sergio (yo)</a></br>
+<a href="https://github.com/draseld/asir1_luis" target="_blank">Luis</a></br>
+<a href="https://github.com/alejandro-vgr/asir176" target="_blank">Alejandro</a></br>
+<a href="https://github.com/serran24/asir1_victor" target="_blank">Victor</a></br>
+<a href="https://github.com/kevin-raico-97" target="_blank">Kevin</a></br>
+<a href="https://github.com/rolandoMH96/Asir1-RMH" target="_blank">Rolando</a></br>
+</body>
