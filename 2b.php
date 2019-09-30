aff7249c161cb160dbceb529bd91a1ff826d4ad2
sergiosamperio <sergiosampe9@gmail.com>
2019-09-30 12:04:47 +0200

    version funciones

---
+++ 2b.php
@@ -0,0 +1,11 @@
+ <?php
+ function calcula () {
+	$a= 18;
+	$b= 2;
+	echo 'Suma'.$a.'+'.$b.'=' . ($a+$b) . '</br>';
+	echo 'Resta'.$a.'-'.$b.'=' . ($a-$b) . '</br>';
+	echo 'Producto'.$a.'*'.$b.'=' . ($a*$b) . '</br>';
+	echo 'División'.$a.'/'.$b.'=' . ($a/$b) . '</br>';
+}
+calcula ();
+?>
