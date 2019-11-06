f857215e91fa922f3142537a892345da7bec9379
sergiosamperio <sergiosampe9@gmail.com>
2019-11-06 09:17:16 +0100

    version html

---
+++ html.php
@@ -0,0 +1,172 @@
+<html>
+<head>
+<title>Real Racing Club | Web Oficial</title>
+</head>
+<body bgcolor="#159528">
+
+<center>
+<img src="racing.png" width="250" height="300"/> 
+
+
+<font face="Cambria"><marginwidth<font size="6"><h1><b>REAL RACING CLUB | Desde 1913</b></h1></marginwidth
+<left></font>
+<font color="white"><h2><u>Clasificación LaLiga Smartbank</u></h2></font>
+</left>
+<table class="egt">
+<TABLE BORDER>
+  <tr>
+  
+    <td>P</td>
+
+    <td>Equipo</td>
+
+    <td>PTS</td>
+	
+	<td>PJ</td>
+
+  </tr>
+
+  <tr>
+
+	
+
+    <td>1</td>
+
+    <td><b>REAL RACING CLUB</b></td>
+
+    <td>87</td>
+	
+	<td>41</td>
+
+  </tr>
+  
+    <tr>
+	
+	
+
+    <td>2</td>
+
+    <td>Cádiz CF</td>
+
+    <td>79</td>
+	
+	<td>41</td>
+
+  </tr>
+  
+  <tr>
+  
+	
+
+    <td>3</td>
+
+    <td>UD Almeria</td>
+
+    <td>74</td>
+	
+	<td>41</td>
+
+  </tr>
+  
+    <tr>
+	
+	
+
+    <td>4</td>
+
+    <td>CF Fuenlabrada</td>
+
+    <td>70</td>
+	
+	<td>41</td>
+
+  </tr>
+  
+    <tr>
+	
+	
+
+    <td>5</td>
+
+    <td>Rayo Vallecano</td>
+
+    <td>69</td>
+	
+	<td>41</td>
+
+  </tr>
+  
+
+    <tr>
+	
+
+
+    <td>6</td>
+
+    <td>Real Zaragoza</td>
+
+    <td>65</td>
+	
+	<td>41</td>
+
+  </tr>
+
+</table>
+
+
+<left>
+<font color="white"><h2><u>Próximo Partido: Jornada 42</u></h2></font>
+<h5>Domingo 24/05/2020</h5>                             
+<h5>19:00 hora local</h5>
+<h5>Campos de Sport El Sardinero</h5>
+<img src="racingrayo.png" width="300" height="200"/> 
+<left>
+
+<font color="white"><h2><u>Entradas</u></h2></font>
+<img src="estadio.png" width="300" height="200"/> 
+<h5>1. Tribuna Central => 50€</h5> 
+<h5>2. Tribuna Principal => 80€</h5> 
+<h5>3. Tribuna Este => 20€</h5> 
+<h5>4. Tribuna Norte => 20€</h5>
+<h5>5. Tribuna Sur => 20€</h5> 
+<h5>6. Preferencia Oeste => 10€</h5> 
+<h5>7. Preferencia Este => 10€</h5> 
+<h5>8. Preferencia Norte => 10€</h5> 
+<h5>9. Preferencia Sur => 10€</h5>
+
+<font color="white"><h2><u>Sorteo de 2 entradas para el partido</u></h2></font>
+<form>
+<p>Nombre: <input type="text" name="nombre" size="20"></p>
+
+<p>Apellidos: <input type="text" name="apellidos" size="40"></p>
+
+<p>DNI: <input type="text" name="DNI" size="9"></p>
+
+<p>Zona: <input type="text" name="zona" size="20"></p
+
+<p>E-mail: <input type="text" name="e-mail" size="40"></p
+
+</form>
+
+<form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+
+<input type="checkbox" name="condiciones" checked="checked" />
+
+Acepto las condiciones del sorteo<br />
+ 
+
+<input type="submit" name="submit" value="Participar"><br>
+
+ 
+
+</form>
+
+
+
+
+
+
+
+
+
+
