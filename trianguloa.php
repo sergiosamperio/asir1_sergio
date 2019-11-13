df7c82cb4b7be036d5862bdaffa0d4b6dc442d4a
sergiosamperio <sergiosampe9@gmail.com>
2019-11-13 09:08:18 +0100

    version triangulo a

--- trianguloa.php
+++ trianguloa.php
@@ -8,9 +8,26 @@ function longitud ($a, $b, $c) {
 						}
 						return $r;
 }
+echo "El triángulo es escaleno"."<br>";
 echo longitud (4, 2, 4). "<br>";
+
+echo "El triángulo es equilatero"."<br>";
 echo longitud (3, 3, 3). "<br>";
-echo longitud (2, 1, 1). "<br>";
+
+echo "El triángulo es isosceles"."<br>";
+echo longitud (2, 2, 1). "<br>";
+
+echo "El triángulo es escaleno"."<br>";
+echo longitud (2, 3, 2). "<br>";
+
+echo "El triángulo es isosceles"."<br>";
+echo longitud (1, 1, 3). "<br>";
+
+echo "El triángulo es escaleno"."<br>";
+echo longitud (2, 1, 2). "<br>";
+
+echo "El triángulo es equilatero"."<br>";
+echo longitud (4, 4, 4). "<br>";
 ?>
 
 		
