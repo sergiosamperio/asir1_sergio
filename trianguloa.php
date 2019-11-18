75866effd1e8033f77827937cff694e9e20983a3
sergiosamperio <sergiosampe9@gmail.com>
2019-11-18 10:13:18 +0100

    version triangulo a

--- trianguloa.php
+++ trianguloa.php
@@ -1,14 +1,14 @@
 <?php
 function longitud ($a, $b, $c) {
-				$r="triangulo escaleno";
-						if ($a==$b || $b==$c) {
-								$r="triangulo isosceles";
-						if ($b==$c and $a==$c)
-								$r="triangulo equilatero";
-						}
-						return $r;
+	$r="triangulo escaleno";
+		if ($a==$b || $a==$c ||$b==$c){
+	$r="triangulo isosceles";
+		if ($b==$c and $a==$c)
+	$r="triangulo equilatero";
 }
-echo "El triángulo es escaleno"."<br>";
+return $r;
+}
+echo "El triángulo es isosceles"."<br>";
 echo longitud (4, 2, 4). "<br>";
 
 echo "El triángulo es equilatero"."<br>";
@@ -17,17 +17,20 @@ echo longitud (3, 3, 3). "<br>";
 echo "El triángulo es isosceles"."<br>";
 echo longitud (2, 2, 1). "<br>";
 
-echo "El triángulo es escaleno"."<br>";
+echo "El triángulo es isosceles"."<br>";
 echo longitud (2, 3, 2). "<br>";
 
-echo "El triángulo es isosceles"."<br>";
-echo longitud (1, 1, 3). "<br>";
-
 echo "El triángulo es escaleno"."<br>";
-echo longitud (2, 1, 2). "<br>";
+echo longitud (3, 4, 5). "<br>";
 
 echo "El triángulo es equilatero"."<br>";
 echo longitud (4, 4, 4). "<br>";
+
+echo "El triángulo es isosceles"."<br>";
+echo longitud (3, 1, 1). "<br>";
+
+echo "El triángulo es escaleno"."<br>";
+echo longitud (6, 5, 4). "<br>";
 ?>
 
 		
