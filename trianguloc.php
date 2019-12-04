09963842e75cb8a8e733df8ad7cd6919814a0e96
sergiosamperio <sergiosampe9@gmail.com>
2019-12-04 09:10:28 +0100

    version triangulo c

--- trianguloc.php
+++ trianguloc.php
@@ -1,27 +1,27 @@
 <?php
 function angulo ($a, $b, $c) {
-				$r="acutangulo";
-						if ($a==90 or $b==90 or $c==90) 
-								$r="rectangulo";
-						if ($a>90 or $b>90 or $c>90)
-								$r="obtusangulo";
-						return $r;
+	$r="acutangulo";
+	if ($a==90 or $b==90 or $c==90) 
+	$r="rectangulo";
+	if ($a>90 or $b>90 or $c>90)
+	$r="obtusangulo";
+	return $r;
 }
 function longitud ($d, $e, $f) {
-				$r="escaleno";
-						if ($d==$e || $e==$f) {
-								$r="isosceles";
-						if ($e==$f and $d==$f)
-								$r="equilatero";
-						}
-						return $r;
+	$r="escaleno";
+	if ($d==$e || $e==$f) {
+	$r="isosceles";
+	if ($e==$f and $d==$f)
+	$r="equilatero";
+	}
+	return $r;
 }
-function C($a, $b, $c, $d, $e, $f) {
+function triangulo($a, $b, $c, $d, $e, $f) {
 	return angulo ($a, $b, $c).' y '.longitud ($d, $e, $f);
 }
-echo C(100, 70, 50, 2, 4, 5). "<br>";
-echo C(80, 30, 70, 3, 4, 4). "<br>";
-echo C(90, 90, 90, 5, 5, 5). "<br>";
+echo triangulo(100, 70, 50, 2, 4, 5). "<br>";
+echo triangulo(80, 30, 70, 3, 4, 4). "<br>";
+echo triangulo(90, 90, 90, 5, 5, 5). "<br>";
 ?>
 
 		
